7dd9dde6f1133af0e5691839f28d9e3947a0f465
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-05-15 20:02:06 -0300

    Funcao para pega a senha do admin

--- desafio1/create_user.py
+++ desafio1/create_user.py
@@ -1,3 +1,16 @@
-#!/usr/bin/evn python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# encoding: utf-8
+import ldap
 import csv
-from passlib.hash import ldap_salted_sha1
+import getpass
+
+########## Constants ##########
+LDAP_HOTS = "ldap://localhost"
+LDAP_BASE_DN = "ou=People,dc=mercadolivre,dc=br"
+LDAP_ADMIN_DN = "cn=admin,dc=mercadolivre,dc=br"
+
+######### Funcoes ##############
+# Ask for LDAP admin password
+def input_ldap_pass():
+    return getpass.getpass("Enter LDAP manager password: ")
