b34dea39fc5b8eb067834d09db0110fdfdcb65d4
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-05-15 21:10:33 -0300

    Funcao de conexao no ldap server

--- desafio1/create_user.py
+++ desafio1/create_user.py
@@ -4,6 +4,8 @@
 import ldap
 import csv
 import getpass
+import sys
+import string
 
 ########## Constants ##########
 LDAP_HOTS = "ldap://localhost"
@@ -15,6 +17,26 @@ LDAP_ADMIN_DN = "cn=admin,dc=mercadolivre,dc=br"
 def input_ldap_pass():
     return getpass.getpass("Enter LDAP manager password: ")
 
+# This will try to bind to LDAP with admin DN and givem password and exit
+# the script with error message if it fails.
+def  try_ldap_bind(admin_pass):
+    try:
+        ldap_conn = ldap.initialize(LDAP_HOST)
+    except ldap.SERVER_DOWN:
+        print("Can't contact LDAP server")
+        exit(4)
+    
+    try:
+        ldap_conn.simple_bind_s(LDAP_ADMIN_DN, admin_pass)
+    except ldap.INVALID_CREDENTIALS:
+        print("This password is incorrect!")
+        sys.exit(3)
+
+    print("Authentization successful")
+    print("")
+
+# Loanding information about user to create
 
 ######### main #############
 admin_pass = input_ldap_pass()
+try_ldap_bind(admin_pass)
