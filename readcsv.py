742c70f96652b75d1d74cbd095069d8daa359f7d
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-05-15 22:07:32 -0300

    Removendo arquivos nao usados

--- readcsv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-print __doc__
-f = csv.reader(open('user.csv'), delimiter=',')
-for [nome, sobrenome, email] in f:
-    print 'nome=%s | sobrenome=%s | email=%s' % (nome, sobrenome, email)
-
-print f.line_num, 'linhas lidas'
-print '--- fim'
