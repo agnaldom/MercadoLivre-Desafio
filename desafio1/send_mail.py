87fed4c01aacc36187fd1fee0e8329cd44e94184
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-05-16 16:44:23 -0300

    removendo codificacao

--- desafio1/send_mail.py
+++ desafio1/send_mail.py
@@ -19,7 +19,7 @@ def get_contacts(filename):
 
     names = []
     emails = []
-    with open(filename, mode='r', encoding='utf-8') as contacts_file:
+    with open(filename, mode='r') as contacts_file:
         for a_contact in contacts_file:
             names.append(a_contact.split()[0])
             emails.append(a_contact.split()[1])
